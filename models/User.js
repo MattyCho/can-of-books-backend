77c4429108fd6699e48b3a7b004e025d4b9de779
MattyCho <myc.chosen@gmail.com>
2021-08-01 18:38:49 -0700

    too many errors

--- models/User.js
+++ models/User.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const mongoose = require('mongoose');
-// const bookSchema = require('./Books.js');
 
 const bookSchema = new mongoose.Schema ({
   name: {type: String, required: true},
   description: {type: String, required: true},
-  status: {type: String}
+  status: {type: String},
+  img: {type: String},
 })
 
 const userSchema = new mongoose.Schema({
-  email: {type: String},
+  email: {type: String, required: true},
   books: [bookSchema]
 })
 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('user', userSchema);
